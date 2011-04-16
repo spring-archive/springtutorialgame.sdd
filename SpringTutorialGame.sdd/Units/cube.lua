3b8ca6505a616aabc7b73bba1675ec9ccf2858d3
anmeldekram71 <anmeldekram71@web.de>
2011-04-16 03:15:29 +0000

    last update did not upload all files it seems files for simplehover

---
+++ SpringTutorialGame.sdd/Units/cube.lua
@@ -0,0 +1,8 @@
+local unitName = "cube"
+
+local unitDef = {
+  name               = "Cube Unit",
+  objectName         = "cube.s3o",
+}
+
+return lowerkeys({ [unitName] = unitDef })
---
+++ SpringTutorialGame.sdd/Units/simplehover.lua
@@ -0,0 +1,33 @@
+local unitName = "simplehover"
+
+local unitDef = {
+name = "Swoosher",
+Description = "A generic tank unit.",
+objectName = "simplehover.s3o",
+script = "simplehover.lua",
+
+maxDamage = 100,
+
+--Movement
+Acceleration = 0.2,
+BrakeRate = 0.3,
+FootprintX = 2,
+FootprintZ = 2,
+MaxSlope = 15,
+MaxVelocity = 8.0,
+MaxWaterDepth = 20,
+MovementClass = "Default2x2",
+TurnRate = 900,
+
+sightDistance = 300,
+
+Builder = 0,
+CanAttack = 0,
+CanGuard = 1,
+CanMove = 1,
+CanPatrol = 1,
+CanStop = 1,
+LeaveTracks = 0, 
+}
+
+return lowerkeys({ [unitName] = unitDef })
