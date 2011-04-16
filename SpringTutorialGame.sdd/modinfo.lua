a67920653f3b00ca6ade93fc5b8e5e08ad07fa2d
anmeldekram71 <anmeldekram71@web.de>
2011-04-16 00:23:41 +0000

    added modinfo.lua added basic folders

---
+++ SpringTutorialGame.sdd/modinfo.lua
@@ -0,0 +1,1 @@
+--http://springrts.com/wiki/Modinfo.lualocal modinfo = {	name = "Spring Tutorial Game",	shortname = "STG",	game = "STG",	shortgame = "STG",	description = "Basic game to help new modders",	url = "code.google.com/p/springtutorialgame",	modtype = "1",	depend = {		"cursors.sdz",		"springcontent.sdz",	}}return modinfo
