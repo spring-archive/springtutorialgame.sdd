3b4e20f7a52b92c9f82074b08c633f7da5fb0824
anmeldekram71 <anmeldekram71@web.de>
2013-07-25 23:41:52 +0000

    http://springrts.com/wiki/ToolBox

--- ToolBox.sdd/modinfo.lua
+++ ToolBox.sdd/modinfo.lua
@@ -1,3 +1,12 @@
+--IMPORTANT NOTE:
+
+--THIS IS NOT THE OFFICIAL SOURCE OF TOOLBOX.
+
+--FIND THE OFFICIAL SOURCE HERE:
+
+--http://springrts.com/wiki/ToolBox
+
+
 -- ToolBox - A package of tools for game and map developers
 -- Authors: CarRepairer, FLOZi, knorke
 -- Thanks: jK (Chili), Tobi (LUS)
@@ -6,7 +15,7 @@ local modinfo = {
 	name			= "ToolBox",
 	shortName		= "TB",
 	version			= "v0.1",
-	game			= "ToolBox",
+	game			= "ToolBox", --SEE NOTE AT TOP OF FILE!
 	shortGame		= "TB",
 	mutator			= "Official",
 	description		= "Useful tools for game development",
